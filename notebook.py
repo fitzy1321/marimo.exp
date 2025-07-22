0ff184cfdb5e6601af98985c5e0fdd2d93013aa5
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2025-07-21 23:47:19 -0600

    clean up

--- notebook.py
+++ notebook.py
@@ -7,18 +7,18 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import os
-    from pathlib import Path
 
     import marimo as mo  # noqa: F401
     import polars as pl
 
-    if "__MISE_ORIG_PATH" not in dict(os.environ):
-        # if not on 'my local'(i.e. has mise installed), get csv from github
-        csv_path = "https://raw.githubusercontent.com/fitzy1321/marimo_exp/refs/heads/main/assets/large_random_data.csv"
-        print("grabbing csv online")
-    else:
-        csv_path = Path("assets/large_random_data.csv")
-        print("using local csv file")
+    # if not on 'my local'(i.e. has mise installed), get csv from github
+    # TODO: need a better way to determine where this is being executed
+    # choices: local, published to ghpages, deployed else where?
+    csv_path = (
+        "https://raw.githubusercontent.com/fitzy1321/marimo_exp/refs/heads/main/assets/large_random_data.csv"
+        if "__MISE_ORIG_PATH" not in dict(os.environ)
+        else "assets/large_random_data.csv"
+    )
 
     polar_df = pl.read_csv(csv_path)
     polar_df
