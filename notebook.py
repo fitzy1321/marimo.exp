1096702528ceb21f6faa8dabf863dd22c244ecff
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2025-07-22 02:08:58 -0600

    update notebook

--- notebook.py
+++ notebook.py
@@ -1,33 +1,13 @@
 import marimo
 
 __generated_with = "0.14.12"
-app = marimo.App(width="medium")
+app = marimo.App(width="medium", app_title="Fitzy1321 Marimo Notebook")
 
 
 @app.cell
 def _():
-    import os
-
     import marimo as mo
-
-    # if not on 'my local'(i.e. has mise installed), get csv from github
-    # TODO: need a better way to determine where this is being executed
-    # choices: local, published to ghpages, deployed else where?
-    csv_path = (
-        "https://raw.githubusercontent.com/fitzy1321/marimo_exp/refs/heads/main/assets/large_random_data.csv"
-        if "__MISE_ORIG_PATH" not in dict(os.environ)
-        else "assets/large_random_data.csv"
-    )
-    return csv_path, mo
-
-
-@app.cell
-def _(csv_path):
-    import polars as pl
-
-    pdf = pl.read_csv(csv_path)
-    pdf
-    return pdf, pl
+    return (mo,)
 
 
 @app.cell(hide_code=True)
@@ -46,6 +26,31 @@ def _(mo):
     """
     )
     return
+
+
+@app.cell
+def _():
+    import os
+
+
+    # if not on 'my local'(i.e. has mise installed), get csv from github
+    # TODO: need a better way to determine where this is being executed
+    # choices: local, published to ghpages, deployed else where?
+    csv_path = (
+        "https://raw.githubusercontent.com/fitzy1321/marimo_exp/refs/heads/main/assets/large_random_data.csv"
+        if "__MISE_ORIG_PATH" not in dict(os.environ)
+        else "assets/large_random_data.csv"
+    )
+    return (csv_path,)
+
+
+@app.cell
+def _(csv_path):
+    import polars as pl
+
+    pdf = pl.read_csv(csv_path)
+    pdf
+    return pdf, pl
 
 
 @app.cell
