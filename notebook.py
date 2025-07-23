b804b4c8b680e15fbfadbd674ce640b1655f954b
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2025-07-22 19:42:02 -0600

    normalize csv file, add some plotting libraries, and try to make my first reactive blocks of code

--- notebook.py
+++ notebook.py
@@ -7,7 +7,6 @@ app = marimo.App(width="medium", app_title="Fitzy1321 Marimo Notebook")
 @app.cell
 def _():
     import marimo as mo
-
     return (mo,)
 
 
@@ -24,6 +23,18 @@ def _(mo):
     Anyway, not sure what this notebook will be next. Maybe I'll find an actually interesting dataset to anaylze and visualize?
 
     Maybe some kind of population data? IDK, some kind of large data set.
+
+    ---
+
+    Found this World Population Dataset, from Kaggle. Link provided [here](https://www.kaggle.com/datasets/jinundjanun/population-world-2024). 
+
+    I have no idea how the data was sourced, method of collect, etc., but this notebook is only an experiment.
+
+    None of the data here should be taken as fact. This is a tech demo of marimo and various notebook UI elements and techniques.
+
+    Also, learning how to use polars with visulation libraries for real. I'm gonna try to make various interactive graphs, plots, charts, etc.
+
+    I'll try to add some methodiology as I go.
     """
     )
     return
@@ -32,7 +43,14 @@ def _(mo):
 @app.cell
 def _():
     import os
+    import polars as pl
+    import altair as alt
+    import plotly as plt
+    return os, pl
 
+
+@app.cell
+def _(os):
     # Choose local csv or remote, depending on ENVs
     csv_path = (
         "assets/world_population.csv"
@@ -43,11 +61,69 @@ def _():
 
 
 @app.cell
-def _(csv_path):
-    import polars as pl
-
+def _(csv_path, pl):
     pdf = pl.read_csv(csv_path, truncate_ragged_lines=True, separator=";")
     pdf
+    return (pdf,)
+
+
+@app.cell
+def _(pdf, pl):
+    # What's the top 10 rated countries? What does 'Rank' meak here?
+    top_10 = pdf.filter(pl.col('Rank').is_in(range(1,11))).sort('Rank')
+    top_10
+    return
+
+
+@app.cell
+def _(pdf, pl):
+    # What's the bottom 20 countries?
+
+    # Get the highest number from 'Rank' col
+    bottom_num = pdf.select(pl.col('Rank').max()).item()
+    bottom_range = range(bottom_num, bottom_num-20, -1)
+    # use predicate to find bottom 10 counties
+    bottom_10 = pdf.filter(pl.col('Rank').is_in(bottom_range)).sort('Rank')
+    bottom_10
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    ## Pie Chart of World Population, with a slider
+
+    - Pie chart of world pop per year.
+    - Slider changes year.
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    # Population_{year} slider
+    year_slider = mo.ui.slider(steps=[2000, 2010, 2015, 2020, 2022, 2024])
+    year_slider
+    return (year_slider,)
+
+
+@app.cell
+def _(pdf, year_slider):
+    # Switches 'Population_{year}' Column string, based on the slider
+    col = f"Population_{year_slider.value}"
+    pdf[col]
+    return
+
+
+@app.cell
+def _():
+    # pie_chart = alt.Chart(pdf).mark_arc().encode(
+    #     theta=col,
+    #     color="Country/Territory"
+    # )
+    # pie_chart
     return
 
 
